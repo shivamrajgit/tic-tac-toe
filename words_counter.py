9b1a2912a7d4fb5de8f818b43e679657673f7351
shivamrajgit <shivambroad@gmail.com>
2024-09-01 20:04:26 +0530

    removed words counter

--- words_counter.py
+++
@@ -1,8 +0,0 @@
-textInput=input("Enter Text:")
-Punctuators=[".",",","!",'\"',"\'"]
-noPunctuators=""
-for i in textInput :
-    if i not in Punctuators:
-        noPunctuators+=i 
-splittedText=noPunctuators.split()
-print(len(splittedText))
